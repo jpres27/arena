a3d1e5ef908b766699282c24cc98a1ecc22fbfd7
jpres <john.presley.27@gmail.com>
2023-11-18 13:03:31 -0500

    implemented poolarena

--- arena.c
+++ arena.c
@@ -11,6 +11,17 @@ typedef struct {
     size_t bytes;
 } Arena;
 
+typedef struct {
+    void *position;
+    uint_fast32_t blockCounter;     // Incremented every time a block is allocated
+    size_t bytes;
+} PoolArena;
+
+inline size_t align(size_t sizeToAlign) {
+    return (sizeToAlign + sizeof(uintptr_t) - 1) & ~(sizeof(uintptr_t) -1);
+}
+
+// Create arena, at least as large as the arg bytes
 Arena createArena(size_t bytes) {
     void *x;
     x = mmap(0, bytes, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
@@ -25,6 +36,20 @@ Arena createArena(size_t bytes) {
     return arena;
 }
 
+// Create an arena with uniformly sized blocks of memory
+// For now, caller is responsible for maintaining a free list
+Arena createPoolArena(size_t bytesPerBlock, size_t blockNum) {
+    size_t bytes = blockNum * (align(bytesPerBlock));
+    void *x; 
+    x = mmap(0, bytes, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+    PoolArena poolarena;
+    poolarena.position = x;
+    poolarena.blockCounter = 0;
+    poolarena.bytes = bytes;
+
+}
+
 int main() {
     printf("%zu", sizeof(uintptr_t));
+    return 0;
 }
