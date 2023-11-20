cb40aea97856ef124380a5bbb818e7391ff62d01
jpres <john.presley.27@gmail.com>
2023-11-20 18:19:04 -0500

    updated header file

--- arena.h
+++ arena.h
@@ -1,5 +1,12 @@
 #ifdef __ARENA_H__
 #define __ARENA_H__
 
+typedef struct Arena;
+typedef struct PoolArena;
+inline size_t align(size_t sizeToAlign);
+Arena createArena(size_t bytes);
+void *arenaAlloc(Arena arena, size_t size);
+PoolArena createPoolArena(size_t bytesPerBlock, size_t blockNum);
+void *poolArenaAlloc(PoolArena poolarena);
 
 #endif
